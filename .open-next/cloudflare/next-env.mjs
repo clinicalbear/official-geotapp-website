8b060658a50b8c6ee44d442390f4065e2a9adb84
Mike <michele.angelo.petraroli@gmail.com>
2025-11-12 19:36:49 +0100

    Updated the pages:build script to npx @opennextjs/cloudflare build

---
+++ .open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY":"pk_test_51SMgBWEKRcnCVjuWcAOLvKCzkM5hvYNssyxifjx9Xlzg40a6Flv2KNx1PUAQ98R9HrYq9es9nxFQqrnLyS5GqbiL00mHvPSB0E","NEXT_PUBLIC_API_BASE":"https://api.geotapp.com"};
+export const development = {"NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY":"pk_test_51SMgBWEKRcnCVjuWcAOLvKCzkM5hvYNssyxifjx9Xlzg40a6Flv2KNx1PUAQ98R9HrYq9es9nxFQqrnLyS5GqbiL00mHvPSB0E","NEXT_PUBLIC_API_BASE":"https://api.geotapp.com"};
+export const test = {};
